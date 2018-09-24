551bcde65c5438d31748575638c9b2c30074d6c8
ZISION <zhouzijian1996@gmail.com>
2018-09-24 19:13:24 +0800

    更新哈希表做法

--- 1_Two_Sum.py
+++ 1_Two_Sum.py
@@ -9,7 +9,25 @@
 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
 """
 
+# 暴力法
+# class Solution:
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         count = len(nums)
+#         for i in range(count):
+#             if nums[i] > target:
+#                 pass
+#             for j in range(i+1, count):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+#                     break
 
+
+# 哈希表
 class Solution:
     def twoSum(self, nums, target):
         """
@@ -17,14 +35,12 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        count = len(nums)
-        for i in range(count):
-            if nums[i] > target:
-                pass
-            for j in range(i+1, count):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-                    break
+        nums_dict = {}
+        for index1, number1 in enumerate(nums):
+            number2 = target - number1
+            if number2 in nums_dict:
+                return nums_dict[number2], index1
+            nums_dict[number1] = index1
 
 # a = Solution()
 # print(a.twoSum(nums=[2, 7, 11, 15], target=9))
